b60d31770ab1a33d7b89c67217c1130c06d5133f
visvoker <s2089533@gmail.com>
2025-01-07 19:28:49 +0800

    Emoji picker component

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -12,6 +12,7 @@ import "quill/dist/quill.snow.css";
 import { Button } from "./ui/button";
 import { Hint } from "./hint";
 import Keyboard from "quill/modules/keyboard";
+import { EmojiPopover } from "./emoji-popover";
 
 type EditorValue = {
   image: File | null;
@@ -128,6 +129,12 @@ const Editor = ({
     }
   };
 
+  const onEmojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+
+    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native)
+  };
+
   const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   console.log({ isEmpty, text });
@@ -147,16 +154,17 @@ const Editor = ({
               <PiTextAa className="size-4" />
             </Button>
           </Hint>
-          <Hint label="Emoji">
+          <EmojiPopover
+            onEmojiSelect={onEmojiSelect}
+          >
             <Button
               disabled={disabled}
               size="iconSm"
               variant="ghost"
-              onClick={() => { }}
             >
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopover>
           <Hint label="Image">
             <Button
               disabled={disabled}
@@ -204,11 +212,16 @@ const Editor = ({
           )}
         </div>
       </div>
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-        <p>
-          <strong>Shift + Return</strong> to add a new line
-        </p>
-      </div>
+      {variant === "create" && (
+        <div className={cn(
+          "p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",
+          !isEmpty && "opacity-100"
+        )}>
+          <p>
+            <strong>Shift + Return</strong> to add a new line
+          </p>
+        </div>
+      )}
     </div >
   );
 };
