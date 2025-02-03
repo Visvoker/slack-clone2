2702c09b95a13092d237b83caadc36f761a16cf8
visvoker <s2089533@gmail.com>
2025-02-04 00:50:57 +0800

    fix loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo } from "react";
 
-import { UserButton } from "@/features/auth/components/user-button";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
@@ -28,9 +28,8 @@ export default function Home() {
   }, [isLoading, workspaceId, router, setOpen]);
 
   return (
-    <div>
-      Login in
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   );
 }
